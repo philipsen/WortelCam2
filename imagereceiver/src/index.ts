8d1b0c161c05ea24ff8ee555676ba1894e4da812
Wim <you@example.com>
2018-09-23 21:35:06 +0200

    cleanup

--- imagereceiver/src/index.ts
+++ imagereceiver/src/index.ts
@@ -1,12 +1,7 @@
-import logger from "./util/logger";
 
-const server = require("./server");
+const server = require('./server');
 const app = server.Server.bootstrap().app;
 
-const io = require('socket.io')(server);
-
-import { Receiver } from "./receiver";
+import { Receiver } from './receiver';
 
 const receiver = Receiver.getInstance();
-
-//export default httpServer;
